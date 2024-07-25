3bd6edfc5f1925fede447962f3ef67a940d15c2d
Curtis Monroe <github@curtismonroe.com>
2024-07-25 17:30:55 +0000

    made pendulums resizable

--- client/src/pendulum.tsx
+++ client/src/pendulum.tsx
@@ -4,9 +4,11 @@ import ReactDOM from 'react-dom/client';
 interface DragElement {
     x: number;
     y: number;
-    active: boolean;
+    radius: number;
     xOffset: number;
     yOffset: number;
+    isRepositioning: boolean;
+    isResizing: boolean;
 }
 
 const Pendulum = (props: { index: number; anchor: number; color: string; cx: number; cy: number; r: number; }) => {
@@ -16,23 +18,25 @@ const Pendulum = (props: { index: number; anchor: number; color: string; cx: num
         {
             x: cx,
             y: cy,
-            active: false,
+            radius: r,
             xOffset: 0,
             yOffset: 0,
+            isRepositioning: false,
+            isResizing: false,
         });
 
-    function handlePointerDown(e: React.PointerEvent<SVGElement>) {
+    function handleMovePendulumPointerDown(e: React.PointerEvent<SVGElement>) {
         const el = e.currentTarget;
         const bbox = e.currentTarget.getBoundingClientRect();
         const x = e.clientX - bbox.left;
         const y = e.clientY - bbox.top;
         el.setPointerCapture(e.pointerId);
 
-        setElement({ ...element, xOffset: x, yOffset: y, active: true });
+        setElement({ ...element, xOffset: x, yOffset: y, isRepositioning: true });
     }
 
-    function handlePointerMove(e: React.PointerEvent<SVGElement>) {
-        if (element.active !== true) {
+    function handleMovePendulumPointerMove(e: React.PointerEvent<SVGElement>) {
+        if (element.isRepositioning !== true) {
             return;
         }
 
@@ -48,7 +52,30 @@ const Pendulum = (props: { index: number; anchor: number; color: string; cx: num
     }
 
     function handlePointerUp(e: React.PointerEvent<SVGElement>) {
-        setElement({ ...element, active: false });
+        setElement({ ...element, isRepositioning: false, isResizing: false });
+    }
+
+    function handleSizePendulumPointerDown(e: React.PointerEvent<SVGElement>) {
+        const el = e.currentTarget;
+        const bbox = e.currentTarget.getBoundingClientRect();
+        const x = e.clientX - bbox.left;
+        const y = e.clientY - bbox.top;
+        el.setPointerCapture(e.pointerId);
+
+        setElement({ ...element, xOffset: x, yOffset: y, isResizing: true });
+    }
+
+    function handleSizePendulumPointerMove(e: React.PointerEvent<SVGElement>) {
+        if (element.isResizing !== true) {
+            return;
+        }
+
+        const bbox = e.currentTarget.getBoundingClientRect();
+        const x = e.clientX - (bbox.left + (bbox.width)/2);
+        const y = e.clientY - (bbox.top + (bbox.height)/2);
+        const radius = Math.sqrt(x*x + y*y);
+
+        setElement({ ...element, radius: radius });
     }
 
     return (
@@ -57,11 +84,20 @@ const Pendulum = (props: { index: number; anchor: number; color: string; cx: num
             <circle id={`pendulum-${index}`}
                 cx={element.x}
                 cy={element.y}
-                r={r}
-                fill={color}
-                onPointerDown={(evt) => handlePointerDown(evt)}
+                r={element.radius}
+                fill={element.isResizing ? 'black' : color}
+                onPointerDown={(evt) => handleSizePendulumPointerDown(evt)}
+                onPointerMove={(evt) => handleSizePendulumPointerMove(evt)}
                 onPointerUp={(evt) => handlePointerUp(evt)}
-                onPointerMove={(evt) => handlePointerMove(evt)}
+            />
+            <circle id={`pendulum-core-${index}`}
+                cx={element.x}
+                cy={element.y}
+                r={element.radius-10}
+                fill={color}
+                onPointerDown={(evt) => handleMovePendulumPointerDown(evt)}
+                onPointerMove={(evt) => handleMovePendulumPointerMove(evt)}
+                onPointerUp={(evt) => handlePointerUp(evt)}
             />
         </>);
 }
